4a0c26e3758e70acf8b65b7a489530d8ee6d9125
CesarOuchi <ck137@hotmail.com>
2022-10-17 01:30:13 -0300

    Add DB

---
+++ src/app/components/event.ts
@@ -0,0 +1,10 @@
+export interface Event {
+    id?: number
+    name: string,
+    production: string,
+    rating: number,
+    image: string,
+    description: string,
+    categories: string[],
+    features: string
+} 
